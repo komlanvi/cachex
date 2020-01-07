4d0f7e858b59833371443bf678ef177413c33324
komlanvi <doncredas@gmail.com>
2020-01-07 11:19:24 +0100

    functional core

---
+++ lib/cachex/core.ex
@@ -0,0 +1,37 @@
+defmodule Cachex.Core do
+  @moduledoc """
+  Contain the business logic to retrieve or put a value from / to a cache db
+  """
+
+  alias Cachex.Value
+
+  def get(cache, key) do
+    case Map.get(cache, key) do
+      nil ->
+        random_val = Enum.random(1..100)
+#        value = %Value{number: random_val, access: 1}
+#        new_cache = Map.put(cache, key, value)
+#        {new_cache, value}
+
+        updated_cache = put(cache, key, random_val)
+        get(updated_cache, key)
+
+      value ->
+        if value.access == 3 do
+          random_val = Enum.random(1..100)
+          new_cache = put(cache, key, random_val)
+
+          get(new_cache, key)
+        else
+          new_value = Map.update!(value, :access, &(&1 + 1))
+          new_cache = Map.put(cache, key, new_value)
+
+          {new_cache, new_value}
+        end
+    end
+  end
+
+  def put(cache, key, number) do
+    Map.put(cache, key, %Value{number: number, access: 0})
+  end
+end
